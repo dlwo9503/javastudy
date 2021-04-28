a5f97d674744cff9ce5a4d1c165da546ec70a5fc
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-04-28 15:21:26 +0900

    객체지향프로그래밍 - 클래스(생성자)

--- chapter03/src/main/java/paint/PaintApp.java
+++ chapter03/src/main/java/paint/PaintApp.java
@@ -6,7 +6,9 @@ public class PaintApp {
 		Point point1 = new Point();
 		point1.setX(10);
 		point1.setY(20);
-		
 		point1.show();
+		
+		Point point2 = new Point(100, 200);
+		point2.show();
 	}
 }
