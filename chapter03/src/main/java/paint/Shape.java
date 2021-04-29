84533dad453a4d6477aaf7df20833fa921de30d9
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-04-29 10:53:22 +0900

    인터페이스 적용#1

--- chapter03/src/main/java/paint/Shape.java
+++ chapter03/src/main/java/paint/Shape.java
@@ -1,6 +1,6 @@
 package paint;
 
-public abstract class Shape { // 추상 클래스이므로 new 객체로 생성 불가능하게 함
+public abstract class Shape implements Drawable{ // 추상 클래스이므로 new 객체로 생성 불가능하게 함
 	private String lineColor;
 	private String fillColor;
 	
