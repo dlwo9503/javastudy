a6e43e2811dfb1d4fd951469ce6c34a766972445
dlwo9503 <74041698+dlwo9503@users.noreply.github.com>
2021-04-30 15:41:17 +0900

    Delete finaltest directory

--- finaltest/src/main/java/prob02/Mul.java
+++
@@ -1,8 +0,0 @@
-package prob02;
-
-public class Mul implements Arithmetic {
-	
-	public int calculate( int a, int b ) {
-		return a * b;
-	}
-}
--- finaltest/src/main/java/prob02/Sub.java
+++
@@ -1,8 +0,0 @@
-package prob02;
-
-public class Sub implements Arithmetic {
-	
-	public int calculate( int a, int b ) {
-		return a - b;
-	}
-}
