717f7cfe0e9f76aa91734b2bd32e645d76be2b9a
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-05-03 13:47:07 +0900

    io #3

---
+++ chapter04/src/main/java/io/PhoneList01.java
@@ -0,0 +1,76 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.StringTokenizer;
+
+public class PhoneList01 {
+
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		try {
+			File file = new File("phone.txt");
+
+			if (!file.exists()) {
+				System.out.println("file not found");
+				return;
+			}
+
+			System.out.println("=========파일 정보=========");
+			System.out.println(file.getAbsolutePath());
+			System.out.println(file.length() + "bytes");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+			System.out.println(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date(file.lastModified())));
+			
+			System.out.println("=========전화번호=========");
+
+			// 1. 기반스트림
+			FileInputStream fis = new FileInputStream(file);
+			
+			// 2. 보조스트림1(byte -> char)
+			InputStreamReader isr = new InputStreamReader(fis);
+			
+			// 3. 보조스트림2(char1|char2|char3|\n -> "char1char2char3")
+			br = new BufferedReader(isr);
+			
+			// 4. 처리
+			String line = null;
+			while((line = br.readLine()) != null) {
+//				System.out.println(line);
+				StringTokenizer st = new StringTokenizer(line, "\t "); // line을 토큰화시키는거 (문자 하나하나씩 쪼갬 \t, " ")
+				
+				int index = 0;
+				while(st.hasMoreTokens()) {
+					String token = st.nextToken();
+					if(index == 0) { // 이름
+						System.out.print(token + ":");
+					} else if (index == 1) { // 전화번호1
+						System.out.print(token + "-");
+						
+					} else if (index == 2) { // 전화번호2
+						System.out.print(token + "-");
+					} else { // 전화번호3
+						System.out.println(token);
+					}
+					index++;
+				}
+			}
+			
+		} catch (IOException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ chapter04/src/main/java/io/PhoneList02.java
@@ -0,0 +1,47 @@
+package io;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class PhoneList02 {
+
+	public static void main(String[] args) {
+		Scanner scanner = null;
+		try {
+			File file = new File("phone.txt");
+
+			if (!file.exists()) {
+				System.out.println("file not found");
+				return;
+			}
+
+			System.out.println("=========파일 정보=========");
+			System.out.println(file.getAbsolutePath());
+			System.out.println(file.length() + "bytes");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+			System.out.println(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date(file.lastModified())));
+			
+			System.out.println("=========전화번호=========");
+			
+			scanner = new Scanner(file); // System.in 자리에 file을 넣을 수도 있음
+			while(scanner.hasNextLine()) { // 다음 라인이 있는가? boolean값
+				String name = scanner.next();
+				String phone1 = scanner.next();
+				String phone2 = scanner.next();
+				String phone3 = scanner.next();
+				System.out.println(name + ":" + phone1 + "-" + phone2 + "-" + phone3);
+			}
+
+		} catch (IOException e) {
+			System.out.println("error:" + e);
+		} finally {
+			if(scanner != null) {
+				scanner.close();
+			}
+		}
+	}
+
+}
