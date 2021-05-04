939ff50255e6a5e9f0b0c7cd332e0b83ead46ca2
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-05-04 14:30:50 +0900

    echo c/s #2

--- chapter04/src/main/java/network/test/TCPClient.java
+++ chapter04/src/main/java/network/test/TCPClient.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class TCPClient {
 	private static final String SERVER_IP = "127.0.0.1";
@@ -19,30 +20,32 @@ public class TCPClient {
 			// 2. Server 연결
 			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
 			System.out.println("[client] connected");
-			
+
 			// 3. IO Stream 받아오기
 			InputStream is = socket.getInputStream();
 			OutputStream os = socket.getOutputStream();
-			
+
 			// 4. 쓰기
 			String data = "Hello World\n";
 			os.write(data.getBytes("utf-8"));
-			
+
 			// 5. 읽기
 			byte[] buffer = new byte[255];
 			int readByteCount = is.read(buffer); // blocking
-			if(readByteCount == -1) {
+			if (readByteCount == -1) {
 				System.out.println("[client] closed by server");
 				return;
 			}
-			
+
 			data = new String(buffer, 0, readByteCount, "utf-8");
 			System.out.println("[client] received : " + data);
+		} catch (SocketException e) {
+			System.out.println("[client] suddenly closed by server");
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if(socket != null && socket.isClosed() == false) {
+				if (socket != null && socket.isClosed() == false) {
 					socket.close();
 				}
 			} catch (IOException e) {
