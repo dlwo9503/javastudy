a96d5a79db13324172b291b44ced7e2ae945e6b6
Lee Jae-Seong <74041698+dlwo9503@users.noreply.github.com>
2021-05-04 10:09:25 +0900

    IPAddress

---
+++ chapter04/src/main/java/network/Localhost.java
@@ -0,0 +1,32 @@
+package network;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Localhost { // 자기 자신
+
+	public static void main(String[] args) {
+		
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			
+			String hostName = inetAddress.getHostName();
+			String hostAdress = inetAddress.getHostAddress();
+			System.out.println(hostName);
+			System.out.println(hostAdress);
+			
+			byte[] addresses = inetAddress.getAddress();
+			for (byte address : addresses) {
+				System.out.print(address & 0x000000ff);
+				System.out.print(".");
+			}
+			
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
